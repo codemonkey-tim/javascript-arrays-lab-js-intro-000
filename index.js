dcf6e3b281291fa5d1e9645f31732127fe73b8e9
TIMOTHY P O'BRIEN <codemonkey.tim@gmail.com>
2019-09-19 19:37:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name){
-
+  kittens.shift(name);
+  return kittens;
 }
 
 function destructivelyPrependKitten(name){
-
+  kittens.unshift(name);
+  return kittens;
 }
 
 function destructivelyRemoveLastKitten(name){
